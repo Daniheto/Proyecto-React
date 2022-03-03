554ce7b3631026273ff96f5edbe486a7ef17743e
Daniel Hernandez Torres <daedheto@gmail.com>
2022-03-03 00:16:32 -0300

    tercer

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import NavBar from "./NavBar";
+
+const Header = () => {
+  return (
+    <header id="main-header">
+      <h1>Hammers Ultimate Club</h1>
+      <NavBar />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import Img from "./Img";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <a href="https://www.google.com/">Google</a>
+      <a href="https://plataforma.coderhouse.com/">Coder</a>
+      <a href="https://www.youtube.com/">YouTube</a>
+      <Img />
+    </nav>
+  );
+};
+
+export default NavBar;
